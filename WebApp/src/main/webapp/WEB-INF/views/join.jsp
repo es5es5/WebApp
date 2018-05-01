762ec203397fba7b019c54dde24abfd635a6b5e5
es5es5 <es5e5s@nate.com>
2018-05-02 01:06:01 +0900

    Join.jsp, jQuery.js, vaildCheck.js
    #2, #7
    
    TO DO.
    1. 리소스 정리
    2. js 이용해서 join.jsp 완성

--- WebApp/src/main/webapp/WEB-INF/views/join.jsp
+++ WebApp/src/main/webapp/WEB-INF/views/join.jsp
@@ -9,13 +9,17 @@
 <script type="text/javascript"
 	src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script src="http://www.modernizr.com/downloads/modernizr-latest.js"></script>
+<script type="text/javascript" src="resources/jquery.js"></script>
+<script type="text/javascript" src="resources/vaildCheck.js"></script>
 <script type="text/javascript" src="resources/index/assets/js/login.js"></script>
+<script type="text/javascript" src="resources/index/assets/js/join.js"></script>
 <script type="text/javascript" src="resources/index/assets/js/check.js"></script>
 
 </head>
 <body>
 	<form id="slick-login" action="join.do" method="post" name="joinForm" onsubmit="return joinCheck();">
-		<label for="username">ID</label><input type="text" name="username" class="placeholder" placeholder="username@jinslover.com" autofocus="autofocus">
+		<label for="name">NAME</label><input type="text" name="username" class="placeholder" placeholder="username@jinslover.com" autofocus="autofocus">
+		<label for="id">ID</label><input type="text" name="username" class="placeholder" placeholder="username@jinslover.com" autofocus="autofocus">
 		<label for="password">PASSWORD</label><input type="password" name="password" class="placeholder" placeholder="password">
 		<label for="password2">PASSWORD check</label><input type="password" name="password2" class="placeholder" placeholder="password check">
 			<input type="submit" value="JOIN">
---
+++ WebApp/src/main/webapp/resources/index/assets/js/join.js
@@ -0,0 +1,62 @@
+function joinCheck() {
+	var idField = document.joinForm.id;
+	var pwField = document.joinForm.password;
+	var pwChkField = document.joinForm.password2;
+	var nameField = document.joinForm.name;
+
+	if ($("#joinIDResult").text() == "ID 중복" || isEmpty(idField)
+			|| containsHangul(idField)) {
+		alert("id 확인");
+		idField.value = "";
+		idField.focus();
+		return false;
+	} else if (isEmpty(pwField) || notEquals(pwField, pwChkField)
+			|| lessThan(pwField, 4) || notContains(pwField, "1234567890")
+			|| notContains(pwField, "qwertyuiopasdfghjklzxcvbnm")
+			|| notContains(pwField, "QWERTYUIOPASDFGHJKLZXCVBNM")) {
+		alert("pw 확인");
+		pwField.value = "";
+		pwChkField.value = "";
+		pwField.focus();
+		return false;
+	} else if (isEmpty(nameField)) {
+		alert("이름 확인");
+		nameField.value = "";
+		nameField.focus();
+		return false;
+	} else if (isEmpty(addr1Field) || isEmpty(addr2Field)
+			|| isEmpty(addr3Field)) {
+		alert("주소 확인");
+		addr1Field.value = "";
+		addr2Field.value = "";
+		addr3Field.value = "";
+		addr3Field.focus();
+		return false;
+	} else if (isEmpty(imgField)
+			|| (isNotType(imgField, ".png") && isNotType(imgField, ".gif")
+					&& isNotType(imgField, ".jpg") && isNotType(imgField,
+					".bmp"))) {
+		alert("사진 확인");
+		imgField.value = "";
+		imgField.focus();
+		return false;
+	}
+	return true;
+}
+function loginCheck() {
+	var idField = document.loginForm.im_id;
+	var pwField = document.loginForm.im_pw;
+
+	if (isEmpty(idField)) {
+		alert("id 확인");
+		idField.value = "";
+		idField.focus();
+		return false;
+	} else if (isEmpty(pwField)) {
+		alert("pw 확인");
+		pwField.value = "";
+		pwField.focus();
+		return false;
+	}
+	return true;
+}
